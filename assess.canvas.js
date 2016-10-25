90673dce6e40ef11dfad369419eef4deed3c5c64
Jacek Kopecky <jacek@jacek.cz>
2016-10-25 23:47:28 +0100

    add two tests

--- assess.canvas.js
+++ assess.canvas.js
@@ -40,27 +40,24 @@ QUnit.test(
       "After retrieving wsarr1, there are 7 elements."
     );
 
-    // todo assert some content
+    assert.equal(
+      window.list1.children[5].textContent,
+      "Return of the Jedi"
+    );
 
-    showList(window.list1, 'http://jacek.soc.port.ac.uk/tmp/ws/arr2');
+
+    showList(window.list2, 'http://jacek.soc.port.ac.uk/tmp/ws/arr2');
 
     assert.equal(
-      window.list1.children.length,
+      window.list2.children.length,
       2,
       "After retrieving wsarr2, there are 2 elements."
     );
 
-    // todo assert check some content
-  }
-);
-
-
-
-QUnit.test(
-
-  "get a simple array from server and put it in a list, updating it every second using setTimeout",
-
-  function (assert) {
+    assert.equal(
+      window.list2.children[1].textContent,
+      "Leia"
+    );
 
   }
 );
@@ -69,9 +66,59 @@ QUnit.test(
 
 QUnit.test(
 
-  "give a sword to Spartacus, using the stick figure code from portsoc/Web-Canvas-Stick-Figures",
+  "Create a function `startShowingMessage` that takes two parameters: an element and a string that is a URL. The function will use `setInterval` to make the following task every 1s: retrieve the URL with an asynchronous request and put the response text into the text content of the provided element.",
 
   function (assert) {
+
+    assert.ok(
+      typeof startShowingMessage === "function",
+      "Create a `startShowingMessage` function."
+    );
+
+    assert.equal(
+      window.message.textContent,
+      '',
+      "Before running the function, the message is empty."
+    );
+
+    startShowingMessage(window.message, 'http://jacek.soc.port.ac.uk/tmp/ws/dyn1');
+
+    var done = assert.async(2);
+
+    setTimeout(checkMessage, 1500);
+    setTimeout(checkMessage, 3000);
+
+    var oldMessage = '';
+    function checkMessage() {
+      assert.notEqual(
+        window.message.textContent,
+        oldMessage,
+        "The message should be changing."
+      );
+      oldMessage = window.message.textContent;
+      done();
+    }
+  }
+);
+
+
+
+QUnit.test(
+
+  "Create a function `drawSpartacus` that takes one parameter, a canvas element. The function will draw the stick figure Spartacus on the provided canvas. Make sure he wields a sword in his hand. For convenience, index.js contains a function `drawStickFigure` that does most of the job, given a canvas element.",
+
+  function (assert) {
+    assert.ok(
+      typeof drawSpartacus === "function",
+      "Create a `drawSpartacus` function."
+    );
+
+    drawSpartacus(window.canvas);
+
+    assert.ok(
+      true,
+      "You need to check with your eyes whether Spartacus is drawn and has a sword."
+    );
 
   }
 );
